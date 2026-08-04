a047d41e4a9bcb0ef2911d288b85d0aea0a85a00
rodrigosrcunha-dotcom <rodrigosrcunha-dotcom@users.noreply.github.com>
2026-08-04 19:16:38 +0000

    deploy: b57ec366a7945af1c5344ccef73ce046ec4ce47f

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="app-container">
-      <h1>Pipeline Frontend</h1>
-      <p>CI/CD configurado para validação, build e deploy automático.</p>
-    </div>
-  );
-}
