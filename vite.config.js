457b1b5981cbddd8b23f5ded8168d4015e4b02cd
rodrigosrcunha-dotcom <rodrigosrcunha@gmail.com>
2026-08-04 15:57:05 -0300

    fix: adiciona base path /Pipeline/ no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Pipeline/',
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js', // ajuste o caminho se necessário
+    setupFiles: './src/setupTests.js',
   },
-});
+})
